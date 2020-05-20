d1d1b135319f82118701f9da493767b052164332
Adrian Santos <adrian.santos@nielsen.com>
2020-05-20 15:06:10 +0200

    Guapisimo

--- src/main/java/com/example/definitivo/cicd/restcontrollers/IndexRestController.java
+++ src/main/java/com/example/definitivo/cicd/restcontrollers/IndexRestController.java
@@ -34,7 +34,7 @@ public class IndexRestController {
     public String getData(){
        try
         {
-            return "Veamos el cambiazo junto al Jenkins guapi";
+            return "Veamos el cambiazo junto al Jenkins guapi guapisimo";
         }
         catch (Exception e)
         {
